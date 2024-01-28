7bc0decedb0e7c64109facfc05f85470ad4df12e
junan <junan76@163.com>
2024-01-28 19:56:47 +0800

    Remove build ouput.
    Signed-off-by: junan <junan76@163.com>

--- build/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jan 26 21:25:33 CST 2024"
-#define LINUX_COMPILE_BY "junan"
-#define LINUX_COMPILE_HOST "x6"
-#define LINUX_COMPILER "gcc version 11.4.0 (Ubuntu 11.4.0-1ubuntu1~22.04) "
