de24984c33db5eff10798712a08a3d9885266e10
junan <junan76@163.com>
2024-01-29 20:56:27 +0800

    "Add debug feature in run_qemu.sh"
    Signed-off-by: junan <junan76@163.com>

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-qemu-system-aarch64 -M virt \
-                    -cpu cortex-a53 \
-                    -kernel $(find build -name Image) \
-                    -drive file=rootfs.ext4,if=none,format=raw,id=hd0 \
-                    -device virtio-blk-device,drive=hd0 \
-                    -append "root=/dev/vda init=/bin/init.sh" \
-                    -nographic
+QEMU=qemu-system-aarch64
+
+QEMU_OPTS=("-M" "virt"
+    "-cpu" "cortex-a53"
+    "-kernel" "$(find build -name Image)"
+    "-drive" "file=rootfs.ext4,if=none,format=raw,id=hd0"
+    "-device" "virtio-blk-device,drive=hd0"
+    "-append" "root=/dev/vda init=/bin/init.sh"
+    "-nographic")
+
+main() {
+    case $1 in
+    r | run)
+        "${QEMU}" "${QEMU_OPTS[@]}"
+        ;;
+    d | debug)
+        "${QEMU}" "${QEMU_OPTS[@]}" -s -S
+        ;;
+    *)
+        echo "run_qemu.sh <run|debug>"
+        ;;
+    esac
+}
+
+main $@
